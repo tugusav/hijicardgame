aec6872736d974ff0c153aff93ef35bcaa662688
dimasanshari <73988822+dimasanshari@users.noreply.github.com>
2021-03-23 21:17:43 +0700

    Update Player, PlayerCards

--- src/Player.java
+++ src/Player.java
@@ -1,26 +1,27 @@
 
-public class Player extends playerCards {
+public class Player {
     private String namaPlayer;
     private boolean statusPlayer;
     private PlayerCardList playerCards; 
 
 
-    public Player(String namaPlayer, PlayerCardList playerCards){
-        
+    public Player(String namaPlayer, PlayerCards playerCards){
+        //Konstruktor dari class player
         this.namaPlayer = namaPlayer;
         this.playerCards = playerCards;
     }
     public String getNamePlayer(){
         return this.namaPlayer;
+        //untuk nge-return nama player yang sedang bermain (giliran)
     }
 
-    public void String showCards(){
+    public void showListCards(){
+        PlayerCards.showListCards();
+         //Ini belum kebayang banget sih tapi gw coba buat implementasi di PlayerCards
+    }
        
-
+    public  void getNumCards(){
+        PlayerCards.getNumCards();
+        //gatau ini wkwkw bener ga ye
     }
-
-    public getNumCard(Card listPlayerCard){
-        jumlahCardPlayer(listPlayerCard);
-    }
-
 }
--- src/PlayerCards.java
+++ src/PlayerCards.java
@@ -1,19 +1,31 @@
 
-public class PlayerCardList extends Card {
+public class PlayerCards extends Player {
     private List<Card> playerCards = new ArrayList<>();
     public int numCards = 0;
-    public void drawCard(Card drawCard){
-
+    public void drawCard(Card drawNCard){
+        playerCards.addAll(drawNCard);
+        //Nambahin berupa list of cards (kalo nambah kartunya banyak)
+    }
+    public void drawCard(Type drawOneCard){
+        playerCards.add(drawOneCard)
+        //Ini bakal add one element di akhir list gitu, reference dari geeks for geeks
+        //Ini belum selese nunggu kelas card selese
     }
 
     public void discardCard(Card currentCard) {
-
+        //ini belum kebayang jujur AWKAOWAKWOAKWO
     }
 
-    public void int jumlahCardPlayer(Card playerCards){
+    public void getNumCards(Card playerCards){
         for(int i =0; i<playerCards.length;i++){
             numCards += 1;
+        //Ini dia iterate nambah terus jumlah kartunya tiap ngedtect ada kartu
         }
         System.out.println("Jumlah kartu %s\n adalah %d\n", namaPlayer, numCards);
+        //Ini bakal ngeluarin output berupa jumlah kartu yang dipegan oleh seorang
+    }
+
+    public void showListCards(){
+        return this.playerCards;
     }
 }
