84ddd79f7e345fb2e7eb37631e5df6374fff44e0
irfan <60288026+ilyasIrfan@users.noreply.github.com>
2021-03-24 17:47:18 +0700

    Update Angka.java

--- src/Angka.java
+++ src/Angka.java
@@ -1,13 +1,26 @@
 public class Angka extends Card{
-//    private final int number;
+    private int number;
 
-    public Angka(int number) {
-        super();
-//        this.number = number;
+    public Angka(int number, Color color) {
+        super(color.getColor(), "ANGKA");
+            if ((number == 1 || number == 2 || number == 3
+                    || number == 4 || number == 5 || number == 6
+                    || number == 7 || number == 8 || number == 9
+                    || number == 0)) {
+                this.number = number;
+            } else {
+                try {
+                    throw new InvalidNumberException(number);
+                } catch (InvalidNumberException e) {
+                    System.err.println(e.getErrorMessage());
+                }
+            }
+    }
+    public int getNumber() {
+        return number;
     }
 
     public boolean equals(Object o){
         return this == o;
-
     }
 }
