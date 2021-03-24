36ff771ee23f9e5e286c34cacbbd3362260c2152
irfan <60288026+ilyasIrfan@users.noreply.github.com>
2021-03-24 17:50:13 +0700

    Add files via upload

---
+++ src/Color.java
@@ -0,0 +1,27 @@
+package Hiji;
+
+public class Color {
+    //Dibuat untuk membuat color tiap kartu
+    private String color = "NULL";
+
+    public Color(String color) {
+        if (color.equals("RED") || color.equals("GREEN")
+                || color.equals("BLUE") || color.equals("YELLOW")){
+            this.color = color;
+        }else{
+            try{
+                throw new InvalidColorException(color);
+            } catch(InvalidColorException e){
+                System.err.println(e.getErrorMessage());
+            }
+        }
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
