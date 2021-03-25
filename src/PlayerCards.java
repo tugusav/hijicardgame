427fd1ba1e6c6e2dd5a85ae2de947dc0c87f5f91
tugusav <18219117@std.stei.itb.ac.id>
2021-03-25 23:02:15 +0700

    fixed inheritance and some syntax errors

--- src/PlayerCards.java
+++ src/PlayerCards.java
@@ -1,31 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public class PlayerCards extends Player {
+public class PlayerCards{
     private List<Card> playerCards = new ArrayList<>();
     public int numCards = 0;
-    public void drawCard(Card drawNCard){
-        playerCards.addAll(drawNCard);
+
+    public void drawCard(List<Card> drawedCards){
+        for(int i = 0; i<drawedCards.size();i++){
         //Nambahin berupa list of cards (kalo nambah kartunya banyak)
+        drawCard(drawedCards.get(i));
+        this.numCards++;
+        }
     }
-    public void drawCard(Type drawOneCard){
-        playerCards.add(drawOneCard)
+    public void drawCard(Card drawCard){
+        playerCards.add(drawCard);
         //Ini bakal add one element di akhir list gitu, reference dari geeks for geeks
         //Ini belum selese nunggu kelas card selese
     }
 
     public void discardCard(Card currentCard) {
         //ini belum kebayang jujur AWKAOWAKWOAKWO
+
     }
 
-    public void getNumCards(Card playerCards){
-        for(int i =0; i<playerCards.length;i++){
-            numCards += 1;
-        //Ini dia iterate nambah terus jumlah kartunya tiap ngedtect ada kartu
-        }
-        System.out.println("Jumlah kartu %s\n adalah %d\n", namaPlayer, numCards);
+    public void printNumCards(Player player){
+        System.out.format("Jumlah kartu %s\n adalah %d\n", player.getNamePlayer() , this.getNumCards());
         //Ini bakal ngeluarin output berupa jumlah kartu yang dipegan oleh seorang
+    }
+    public int getNumCards(){
+        return this.numCards;
     }
 
     public void showListCards(){
-        return this.playerCards;
+        for(int i = 1; i <= playerCards.size(); i++){
+            System.out.println(i + ". " + playerCards.get(i).getType());
+        }
     }
 }
