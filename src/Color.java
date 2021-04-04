e5f6b26b72a192a1d331c9653f85aad52e5cd259
irfan <60288026+ilyasIrfan@users.noreply.github.com>
2021-04-04 10:19:37 +0700

    Update Color.java

--- src/Color.java
+++ src/Color.java
@@ -3,24 +3,22 @@
 public class Color {
     //Dibuat untuk membuat color tiap kartu
     private String color = "NULL";
+    private boolean trueColor;
 
     public Color(String color) {
         if (color.equals("RED") || color.equals("GREEN")
                 || color.equals("BLUE") || color.equals("YELLOW")
                 || color.equals("WILD")){
             this.color = color;
-        }else{
-            try{
-                throw new InvalidColorException(color);
-            } catch(InvalidColorException e){
-                System.err.println(e.getErrorMessage());
-            }
+            trueColor = true;
         }
     }
 
     public void setColor(String color) {
         this.color = color;
     }
+
+    public boolean getColorCard(){return trueColor;}
 
     public String getColor() {
         return color;
